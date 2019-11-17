ab94f68624046c9910dae8fe94b95fd9c9b12524
Eduardo H <edu.slhenrique@gmail.com>
2019-11-17 14:28:15 -0300

    Iniciando Projeto

--- database/database.js
+++ database/database.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize');
 const connection = new Sequelize('guiapress','root','1234',{
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: '-03:00'
 });
 module.exports = connection;
--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const port = 3000;
+const port = 3002;
 const connection = require('./database/database');
 const categoriesController = require('./categories/CategoriesControler')
 const articlesController = require('./article/ArticlesControler')
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 
 app.listen(port, ()=>{
-    console.log('O servidor esta rodando!');
+    console.log(`O servidor esta rodando! Na porta ${port}`);
 });
 //conexao com o banco
 connection
@@ -29,3 +29,52 @@ connection
 //rotas
 app.use('/',categoriesController)
 app.use('/',articlesController)
+app.get('/',(req,res)=>{
+    Article.findAll({
+        order:[
+            ['id','DESC']
+        ]
+    }).then(articles=>{
+        Category.findAll().then(categories=>{
+            res.render('index.ejs', {articles:articles, categories: categories})
+        })
+        
+    })
+    
+})
+app.get('/:slug',(req,res)=>{
+    let slug = req.params.slug;
+    Article.findOne({
+        where:{
+            slug : slug
+        }
+    }).then(article=>{
+        if (article != undefined){
+            Category.findAll().then(categories=>{
+                res.render('article.ejs', {article:article, categories: categories})
+        })} else {
+            res.redirect('/')
+        }
+    }).catch(error =>{
+        res.redirect('/')
+    })
+})
+app.get('/category/:slug',(req,res)=>{
+    let slug = req.params.slug;
+    Category.findOne({
+        where:{
+            slug:slug
+        },
+        include: [{model: Article}]
+    }).then(category=>{
+        if (category != undefined){
+            Category.findAll().then(categories =>{
+                res.render('index.ejs',{articles: category.articles, categories: categories})
+            })
+        } else {
+            res.redirect('/')
+        }
+    }).catch(error=>{
+        res.redirect('/')
+    })
+})
